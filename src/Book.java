eed3adc9e18c434d8546c3c6094d1102ab20b2b8
ResistorD <resistord@gmail.com>
2024-12-22 19:02:22 +0400

    Домашка готова

---
+++ src/Book.java
@@ -0,0 +1,49 @@
+import java.util.Objects;
+public class Book {
+    private String title;
+    private Author author;
+    private int publicationYear;
+
+    public Book(String title, Author author, int publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    @Override
+    public String toString() {
+        // Делегируем вызов toString автора
+        return "Книга: '" + title + "', Автор: " + author.toString() + ", Год: " + publicationYear;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Book)) return false;
+        Book book = (Book) obj;
+        return publicationYear == book.publicationYear &&
+                Objects.equals(title, book.title) &&
+                Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author, publicationYear);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,26 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Домашка 7.1");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Author author1 = new Author("Александр", "Пушкин");
+        Author author2 = new Author("Лев", "Толстой");
+
+        Book book1 = new Book("Евгений Онегин", author1, 1833);
+        Book book2 = new Book("Война и мир", author2, 1869);
+
+        System.out.println("Книга: " + book1.getTitle() + ", Автор: " +
+                book1.getAuthor().getFirstName() + " " +
+                book1.getAuthor().getLastName() + ", Год: " +
+                book1.getPublicationYear());
+
+        System.out.println("Книга: " + book2.getTitle() + ", Автор: " +
+                book2.getAuthor().getFirstName() + " " +
+                book2.getAuthor().getLastName() + ", Год: " +
+                book2.getPublicationYear());
+
+        book1.setPublicationYear(1834);
+        System.out.println("Измененный год публикации книги \"" + book1.getTitle() + "\": " +
+                book1.getPublicationYear());
     }
 }
